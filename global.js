f8dd208bc493efea03ddb35fd900b19d805e6120
sopannair <sopan.nair@gmail.com>
2025-10-17 19:20:38 -0700

    Completed Step 3
    Removed manual nav bars, added automatic nav bar via global.js

--- global.js
+++ global.js
@@ -3,3 +3,40 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+
+// currentLink?.classList.add('current');
+
+const BASE_PATH =
+  location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+    ? '/' // Local server
+    : '/portfolio/'; // GitHub Pages repo name
+
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'cv/', title: 'CV' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  // next step: create link and add it to nav
+  if (!url.startsWith('http')) {
+  url = BASE_PATH + url;
+}
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
+
+a.classList.toggle(
+  'current',
+  a.host === location.host && a.pathname === location.pathname,
+);
