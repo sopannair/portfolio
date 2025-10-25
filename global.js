409571ad4a38c9468f945b3eb53440bbcf8e268a
sopannair <sopan.nair@gmail.com>
2025-10-24 22:15:13 -0700

    lab 4 up to 1.4

--- global.js
+++ global.js
@@ -76,3 +76,31 @@ a.classList.toggle(
   a.host === location.host && a.pathname === location.pathname,
 );
 
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(project, containerElement) {
+  // Your code will go here
+  containerElement.innerHTML = '';
+  const article = document.createElement('article');
+  article.innerHTML = `
+    <h3>${project.title}</h3>
+    <img src="${project.image}" alt="${project.title}">
+    <p>${project.description}</p>
+`;
+  containerElement.appendChild(article);
+
+
+
+}
